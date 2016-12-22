b04efd38a9c115b7fa7367262351976908d5ee24
ROCKLANDGEAR\abhishek <abhishekjain.cs99@gmail.com>
2016-12-22 12:55:48 +0530

    mailertest

--- views/mycntrl.js
+++ views/mycntrl.js
@@ -1,7 +1,17 @@
+use 'strict';
 angular.module('HelloWorldApp', [])
         .controller('HelloWorldController', function ($scope) {
-    $this.sendmail=function(){
-        alert($scope.mail.to);
+    $scope.sendmail=function(){
+        $http({
+  method: 'POST',
+  url: '/'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
     }
     
     
